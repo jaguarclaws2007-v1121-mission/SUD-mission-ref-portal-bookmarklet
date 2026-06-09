890382069f711cc7bebdc91d6c51f92d2acca7e7
jaguarclaws2007-v1121-mission <corbin.mounteer@missionary.org>
2026-06-09 15:06:34 -0600

    Update booklet.js
    add preload code

--- booklet.js
+++ booklet.js
@@ -1,1 +1,5 @@
-
+javascript: (function(){
+  const bookmarkletScript = document.createElement("script");
+  bookmarkletScript.src = "https://cdn.jsdelivr.net/gh/jaguarclaws2007-v1121-mission/SUD-mission-ref-portal-bookmarklet@main/refmanager_booklet%20v1.0.min.js";
+  document.head.append(bookmarkletScript);
+})();
